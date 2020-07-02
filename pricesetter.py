05e50e386661e461a9746600fa0b871a49a676f9
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-02 14:09:21 +0100

    add brainstorming comment

---
+++ pricesetter.py
@@ -0,0 +1,7 @@
+
+# More than 1 customer -> etc customer1, customer2, customer3, .. customerN
+# Each has their own budget -> customer1_budget, customer2_budget, customer3_budget, .. customerN_budget
+# if declared_price <= customer_budget then buy, else not buy
+# declared_price must be price that brings the most revenue
+# algorithm to set declared_price:
+#   soritng algorithm

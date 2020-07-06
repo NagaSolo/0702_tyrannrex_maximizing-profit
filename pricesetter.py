77ad00491e846e9102d2d58d11474e43da3e5522
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-06 11:43:11 +0100

    patching failed

--- pricesetter.py
+++ pricesetter.py
@@ -5,6 +5,19 @@
 # declared_price must be price that brings the most revenue
 # greedy approach algorithm to set declared_price
 
+# calculate all maximum price for each budget
+def get_maximum_price_for_each(list_of_budget):
+    count = 0
+    for budget in range(len(list_of_budget) - 1):
+        if list_of_budget[budget] <= list_of_budget[budget] + 1:
+            count += 1
+        # elif list_of_budget[budget] >= list_of_budget[budget] + 1:
+        #     count -= 1
+        print(count)
+    #print(count)
+        # list_of_budget[i]*len(list_of_budget)
+
 N = int(input())
 for customer_budget in range(N):
-    customer_budget_input = int(input())
+    customer_budget_input = list(input())
+get_maximum_price_for_each(customer_budget_input)

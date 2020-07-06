22b7d972f6839927ce4dc5611f08f1313602b424
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-06 10:43:04 +0100

    add ignored files, loop for input and test

--- pricesetter.py
+++ pricesetter.py
@@ -3,5 +3,8 @@
 # Each has their own budget -> customer1_budget, customer2_budget, customer3_budget, .. customerN_budget
 # if declared_price <= customer_budget then buy, else not buy
 # declared_price must be price that brings the most revenue
-# algorithm to set declared_price:
-#   soritng algorithm
+# greedy approach algorithm to set declared_price
+
+N = int(input())
+for customer_budget in range(N):
+    customer_budget_input = int(input())

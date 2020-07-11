8cee8e170571b00cdb6dd565889282a8920a51d3
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-11 08:04:23 +0100

    remove; unnecessary lines

--- pricesetter.py
+++ pricesetter.py
@@ -8,7 +8,7 @@
 # calculate all maximum price for each budget
 def get_maximum_price_for_each(list_of_budget):
     count = 0
-    for budget in range(len(list_of_budget) - 1):
+    for budget in range(len(list_of_budget)):
         if list_of_budget[budget] <= list_of_budget[budget] + 1:
             count += 1
         # elif list_of_budget[budget] >= list_of_budget[budget] + 1:
@@ -22,7 +22,7 @@ customer_budget_list = []
 for customer_budget in range(N):
     customer_budget_list.append(int(input()))
 
-customer_budget_list.sort()
+customer_budget_list.sort() # sort list inplace
 print(customer_budget_list)
 
 # # pseudo case for minimum value
@@ -40,12 +40,3 @@ print(customer_budget_list)
     if a[n+1] != min; a[n+1]*(len(a)-n) 
 
 """
-for i in range(len(customer_budget_list)):
-    minimum_price = min(customer_budget_list)
-    if customer_budget_list[i] == minimum_price:
-        print(customer_budget_list[i]*len(customer_budget_list))
-    elif customer_budget_list[i] == customer_budget_list[i-1]:
-        print(customer_budget_list[i]*(len(customer_budget_list[i-1:])))
-    else:
-        print(customer_budget_list[i]*(len(customer_budget_list)-i))
-# get_maximum_price_for_each(customer_budget_input)
